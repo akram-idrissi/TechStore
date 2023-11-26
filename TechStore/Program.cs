a82e365ad614ab0edc70b42935b3ae134b174ebc
Akram Idrissi <akram.mo.gb@gmail.com>
2023-11-26 21:29:12 +0100

    Another untrackable changes

--- TechStore/Program.cs
+++ TechStore/Program.cs
@@ -1,3 +1,5 @@
+using TechStore.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,12 +7,30 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+builder.Services.AddDbContext<MovieContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<MovieContext>();
+    context.Database.EnsureCreated();
+    // DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
