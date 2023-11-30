15893a20efb843797d0212eb4289b5142ffd3ace
Akram Idrissi <akram.mo.gb@gmail.com>
2023-11-30 22:58:51 +0100

    Add a data context

---
+++ TechStore/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using TechStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace TechStore.Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> users { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
