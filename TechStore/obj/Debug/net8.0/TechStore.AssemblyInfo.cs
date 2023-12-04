b113ef42292ec6096f859230846b3d22addc83cd
Akram Idrissi <akram.mo.gb@gmail.com>
2023-12-04 01:22:38 +0100

    Unknown changes

--- TechStore/obj/Debug/net8.0/TechStore.AssemblyInfo.cs
+++ TechStore/obj/Debug/net8.0/TechStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+363fb671aa44cd3aed67f0f42e4d02576f43d060")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
