a1432aeff7b5706df328458795d3572243a58a64
Akram Idrissi <akram.mo.gb@gmail.com>
2023-12-09 16:46:34 +0100

    Chnaged the controllers logic

--- TechStore/Controllers/HomeController.cs
+++ TechStore/Controllers/HomeController.cs
@@ -1,7 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using TechStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TechStore.Data;
+using TechStore.Models;
 
 namespace TechStore.Controllers
 {
@@ -20,6 +21,17 @@ namespace TechStore.Controllers
             IQueryable<Category> query = _context.Categories;
             IEnumerable<Category> categories = query.ToList();
             ViewBag.categories = categories;
+
+            var allProducts = _context.products.Include(p => p.category).ToList();
+
+            var random = new Random();
+            var shuffledProducts = allProducts.OrderBy(x => random.Next()).ToList();
+
+            var numberOfFeaturedProducts = 5; 
+            var featuredProducts = shuffledProducts.Take(numberOfFeaturedProducts).ToList();
+
+            ViewBag.featuredProducts = featuredProducts;
+
             return View();
         }
 
@@ -31,7 +43,12 @@ namespace TechStore.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(
+                new ErrorViewModel
+                {
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                }
+            );
         }
     }
 }
--- TechStore/Controllers/ProductController.cs
+++ TechStore/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TechStore.Data;
 using TechStore.Models;
 
@@ -12,16 +13,25 @@ namespace TechStore.Controllers
         public ProductController(DataContext context)
         {
             _context = context;
-        } 
-
-        public IActionResult Product()
-        {
-            return View();
         }
 
-        public IActionResult ProductDetail(int id)
+        public IActionResult Product(int id)
         {
-            var product = _context.products.Where(p => p.id == id).FirstOrDefault();
+            var product = _context
+                .products
+                .Include(p => p.category)
+                .FirstOrDefault(p => p.id == id);
+
+            var similarProducts = _context
+                .products
+                .Where(
+                    p => p.categoryID == product.categoryID && p.id != product.id
+                )
+                .OrderBy(x => Guid.NewGuid()) 
+                .Take(4)
+                .ToList();
+
+            ViewBag.similars = similarProducts;
             return View(product);
         }
     }
